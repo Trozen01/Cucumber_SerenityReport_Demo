81bd5ee5f90bb7272570c30fa71b7859a5438304
kit <sawant.jitendra0@gmail.com>
2021-06-04 02:59:21 +0530

    3rd commit

--- src/test/java/Runnner/TestRunner.java
+++ src/test/java/Runnner/TestRunner.java
@@ -1,19 +1,24 @@
 package Runnner;
 
 import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import net.serenitybdd.cucumber.CucumberWithSerenity;
 
 
-@RunWith(CucumberWithSerenity.class)
+
+@RunWith(Cucumber.class)
 @CucumberOptions(
-		plugin={"pretty"},
-		glue="StepDefiniations",
 		features="C:/Jitu/Work/SoftwareTesting/Practical/Cucumber_SerenityReport_Demo/src/test/resources/Features/Amazon.feature",
-				
-		
-		monochrome=true
-		
+		glue="StepDefiniations",
+		monochrome=true,                                         // if this option is true then all console line is come in black format.
+		//tags ="not @Skip",                                     // it will use for not execute particular scenario. 
+		plugin={"pretty","html:target/HtmlReport/report.html",
+		                 "junit:target/JunitReport/report.xml",
+		                 "json:target/JsonReport/report.json",
+		                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",   
+		                 "rerun:target/failedrerun.txt"      // It will make a entry of that particular scenario in txt file which scenario is failed. may be because internet issue and time issue etc. 
+		}		
 		)
 public class TestRunner {
 
